41d59c9073b0f54b3bd3b9c09c4d66b024bc9c2f
James Lee <jameleetutorial@gmail.com>
2016-12-29 04:21:00 +0000

    improve world count example and add collect example

--- src/main/java/com/sparkTutorial/rdd/WorldCount.java
+++ src/main/java/com/sparkTutorial/rdd/WorldCount.java
@@ -1,26 +1,30 @@
 package com.sparkTutorial.rdd;
 
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple2;
 
 import java.util.Arrays;
+import java.util.Map;
 
 public class WorldCount {
 
     public static void main(String[] args) throws Exception {
-        // Create a Java Spark Context.
-        SparkConf conf = new SparkConf().setAppName("wordCount").setMaster("local[3]");
+
+        Logger.getLogger("org").setLevel(Level.OFF);
+        SparkConf conf = new SparkConf().setAppName("wordCounts").setMaster("local[3]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> lines = sc.textFile("in/word_count.text");
         JavaRDD<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
 
-        JavaPairRDD<String, Integer> counts = words.mapToPair(w -> new Tuple2<>(w, 1))
-                                                   .reduceByKey((x, y) -> x + y);
-        counts.saveAsTextFile("out/word_count.text");
+        Map<String, Long> wordCounts = words.countByValue();
+
+        for (Map.Entry<String, Long> entry : wordCounts.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
     }
 }
---
+++ src/main/java/com/sparkTutorial/rdd/collect/CollectExample.java
@@ -0,0 +1,27 @@
+package com.sparkTutorial.rdd.collect;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectExample {
+
+    public static void main(String[] args) throws Exception {
+        Logger.getLogger("org").setLevel(Level.OFF);
+        SparkConf conf = new SparkConf().setAppName("collect").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<String> inputWords = Arrays.asList("spark", "hadoop", "spark", "hive", "pig", "cassandra", "hadoop");
+        JavaRDD<String> wordRdd = sc.parallelize(inputWords);
+        List<String> words = wordRdd.collect();
+
+        for (String  word : words) {
+            System.out.println(word);
+        }
+    }
+}
