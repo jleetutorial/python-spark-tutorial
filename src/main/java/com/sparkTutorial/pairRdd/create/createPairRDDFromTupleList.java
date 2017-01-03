b17879d27aabab8356c36f08be530c292d6ff59f
James Lee <jameleetutorial@gmail.com>
2017-01-03 20:46:20 +0000

    add createPairRDDFromTupleList

---
+++ src/main/java/com/sparkTutorial/pairRdd/create/createPairRDDFromTupleList.java
@@ -0,0 +1,27 @@
+package com.sparkTutorial.pairRdd.create;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class createPairRDDFromTupleList {
+
+    public static void main(String[] args) throws Exception {
+
+        SparkConf conf = new SparkConf().setAppName("create").setMaster("local[*]");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<Tuple2<String, Integer>> tuple = Arrays.asList(new Tuple2<>("Lily", 23),
+                                                            new Tuple2<>("Jack", 29),
+                                                            new Tuple2<>("Mary", 29),
+                                                            new Tuple2<>("James",8));
+
+        JavaPairRDD<String, Integer> pairRDD = sc.parallelizePairs(tuple);
+        pairRDD.saveAsTextFile("out/pair_rdd_from_tuple_list");
+    }
+}
