8510f26c3bb5c1bfd44638ea5244a04c8af09ced
James Lee <jameleetutorial@gmail.com>
2017-01-07 03:20:24 +0000

    add airport problem and solution for upper case mapping

---
+++ src/main/java/com/sparkTutorial/pairRdd/mapValues/AirportsSolution.java
@@ -0,0 +1,31 @@
+package com.sparkTutorial.pairRdd.mapValues;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+public class AirportsSolution {
+
+    public static void main(String[] args) throws Exception {
+
+        SparkConf conf = new SparkConf().setAppName("airports").setMaster("local[*]");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> airportsRDD = sc.textFile("in/airports.text");
+
+        JavaPairRDD<String, String> airportPairRDD = airportsRDD.mapToPair(getAirportNameAndCountryNamePair());
+
+        JavaPairRDD<String, String> upperCase = airportPairRDD.mapValues(countryName -> countryName.toUpperCase());
+
+        upperCase.saveAsTextFile("out/airports_pair_rdd_uppercase.text");
+    }
+
+    private static PairFunction<String, String, String> getAirportNameAndCountryNamePair() {
+        return (PairFunction<String, String, String>) line -> new Tuple2<>(line.split(",")[1],
+                                                                           line.split(",")[3]);
+    }
+}
