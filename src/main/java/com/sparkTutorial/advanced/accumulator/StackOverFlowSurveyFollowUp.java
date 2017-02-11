86eee4e41c15b9d1b6bbe5c2c6e49554fab6973f
James Lee <jameleetutorial@gmail.com>
2017-02-11 12:20:37 +0000

    add StackOverFlowSurveyFollowUp

--- src/main/java/com/sparkTutorial/advanced/accumulator/StackOverFlowSurveyFollowUp.java
+++ src/main/java/com/sparkTutorial/advanced/accumulator/StackOverFlowSurveyFollowUp.java
@@ -4,11 +4,10 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.util.AccumulatorV2;
 import org.apache.spark.util.LongAccumulator;
 import scala.Option;
 
-public class StackOverFlowSurvey {
+public class StackOverFlowSurveyFollowUp {
 
     public static void main(String[] args) throws Exception {
 
@@ -20,13 +19,18 @@ public class StackOverFlowSurvey {
 
         final LongAccumulator total = new LongAccumulator();
         final LongAccumulator missingSalaryMidPoint = new LongAccumulator();
+        final LongAccumulator processedBytes = new LongAccumulator();
 
         total.register(sparkContext, Option.apply("total"), false);
         missingSalaryMidPoint.register(sparkContext, Option.apply("missing salary middle point"), false);
+        processedBytes.register(sparkContext, Option.apply("Processed bytes"), true);
 
         JavaRDD<String> responseRDD = javaSparkContext.textFile("in/2016-stack-overflow-survey-responses.csv");
 
         JavaRDD<String> responseFromCanada = responseRDD.filter(response -> {
+
+            processedBytes.add(response.getBytes().length);
+
             String[] splits = response.split(",", -1);
 
             total.add(1);
@@ -40,6 +44,7 @@ public class StackOverFlowSurvey {
         });
 
         System.out.println("Count of responses from Canada: " + responseFromCanada.count());
+        System.out.println("Number of bytes processed: " + processedBytes.value());
         System.out.println("Total count of responses: " + total.value());
         System.out.println("Count of responses missing salary middle point: " + missingSalaryMidPoint.value());
     }
