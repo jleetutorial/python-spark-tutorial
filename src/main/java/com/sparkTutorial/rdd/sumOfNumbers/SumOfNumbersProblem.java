0b0c3f817c66f083be1380f85f7905434883bb3d
James Lee <jameleetutorial@gmail.com>
2017-03-11 13:37:16 +0000

    update SumOfNumbersProblem and SumOfNumbersSolution

---
+++ src/main/java/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.java
@@ -0,0 +1,13 @@
+package com.sparkTutorial.rdd.sumOfNumbers;
+
+public class SumOfNumbersProblem {
+
+    public static void main(String[] args) throws Exception {
+
+        /* Create a Spark program to read the first 100 prime numbers from in/prime_nums.text,
+           print the sum of those numbers to console.
+
+           Each row of the input file contains 10 prime numbers separated by spaces.
+         */
+    }
+}
--- src/main/java/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersSolution.java
+++ src/main/java/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersSolution.java
@@ -1,24 +1,25 @@
-package com.sparkTutorial.rdd.sumOfPrimeNums;
+package com.sparkTutorial.rdd.sumOfNumbers;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 import java.util.Arrays;
 
-public class SumOfFirst100PrimeNumbersSolution {
+public class SumOfNumbersSolution {
 
     public static void main(String[] args) throws Exception {
+        Logger.getLogger("org").setLevel(Level.OFF);
 
         SparkConf conf = new SparkConf().setAppName("primeNumbers").setMaster("local[*]");
-
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> lines = sc.textFile("in/prime_nums.text");
-
         JavaRDD<String> numbers = lines.flatMap(line -> Arrays.asList(line.split("\\s+")).iterator());
 
-        JavaRDD<String> validNumbers = numbers.filter(number -> !number.equals(""));
+        JavaRDD<String> validNumbers = numbers.filter(number -> !number.isEmpty());
 
         JavaRDD<Integer> intNumbers = validNumbers.map(number -> Integer.valueOf(number));
 
