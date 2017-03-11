87fe8cec4a5e808d7b250cec9749c39ee9284749
James Lee <jameleetutorial@gmail.com>
2017-03-11 16:34:14 +0000

    improve AirportsNotInUsaProblem

--- src/main/java/com/sparkTutorial/pairRdd/filter/AirportsNotInUsaProblem.java
+++ src/main/java/com/sparkTutorial/pairRdd/filter/AirportsNotInUsaProblem.java
@@ -6,7 +6,7 @@ public class AirportsNotInUsaProblem {
 
         /* Create a Spark program to read the airport data from in/airports.text;
            generate a pair RDD with airport name being the key and country name being the value.
-           Then remove all the airports which are NOT located in United States and output the pair RDD to out/airports_not_in_usa_pair_rdd.text
+           Then remove all the airports which are located in United States and output the pair RDD to out/airports_not_in_usa_pair_rdd.text
 
            Each row of the input file contains the following columns:
            Airport ID, Name of airport, Main city served by airport, Country where airport is located,
