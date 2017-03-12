858fca1204e7e19c225db0d64e73b1a7e4a3d0e4
James Lee <jameleetutorial@gmail.com>
2017-03-12 07:28:20 +0000

    improve AirportsByCountrySolution

--- src/main/java/com/sparkTutorial/pairRdd/groupbykey/AirportsByCountrySolution.java
+++ src/main/java/com/sparkTutorial/pairRdd/groupbykey/AirportsByCountrySolution.java
@@ -1,6 +1,9 @@
 package com.sparkTutorial.pairRdd.groupbykey;
 
+import com.google.common.collect.Iterables;
 import com.sparkTutorial.rdd.commons.Utils;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -8,12 +11,13 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
+import java.util.Arrays;
+
 public class AirportsByCountrySolution {
 
     public static void main(String[] args) throws Exception {
-
+        Logger.getLogger("org").setLevel(Level.ERROR);
         SparkConf conf = new SparkConf().setAppName("airports").setMaster("local[*]");
-
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> lines = sc.textFile("in/airports.text");
@@ -26,11 +30,11 @@ public class AirportsByCountrySolution {
         JavaPairRDD<String, Iterable<String>> AirportsByCountry = CountryAndAirportNameAndPair.groupByKey();
 
         for (Tuple2<String, Iterable<String>> airports : AirportsByCountry.collect()) {
-            System.out.print(airports._1() + " : [");
-            for (String s : airports._2()) {
-                System.out.print(s + ", ");
-            }
-            System.out.println("]");
+            System.out.println(airports._1() + " : " + iterableToString(airports._2()));
         }
+    }
+
+    private static String iterableToString(Iterable<String> iterable) {
+        return Arrays.toString(Iterables.toArray(iterable, String.class));
     }
 }
