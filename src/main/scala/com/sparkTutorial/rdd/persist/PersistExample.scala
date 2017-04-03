1da6b0c1e27d7d2a657c001a6d517b2eae6b7537
James Lee <jameleetutorial@gmail.com>
2017-04-03 23:04:11 +0100

    adopt PersistExample

---
+++ src/main/scala/com/sparkTutorial/rdd/persist/PersistExample.scala
@@ -0,0 +1,23 @@
+package com.sparkTutorial.rdd.persist
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
+import org.apache.spark.storage.StorageLevel
+
+object PersistExample {
+
+    def main(args: Array[String]) {
+        Logger.getLogger("org").setLevel(Level.ERROR)
+        val conf = new SparkConf().setAppName("reduce").setMaster("local[*]")
+        val sc = new JavaSparkContext(conf)
+
+        val inputIntegers = List(1, 2, 3, 4, 5)
+        val integerRdd = sc.parallelize(inputIntegers)
+
+        integerRdd.persist(StorageLevel.MEMORY_ONLY)
+
+        integerRdd.reduce((x, y) => x * y)
+        integerRdd.count()
+    }
+}
