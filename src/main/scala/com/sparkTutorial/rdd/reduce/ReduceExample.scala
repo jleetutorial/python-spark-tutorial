cb6d620378bffdc9fc3785e2b1dbd57e2e20b2c7
James Lee <jameleetutorial@gmail.com>
2017-04-03 23:00:28 +0100

    adopt ReduceExample

---
+++ src/main/scala/com/sparkTutorial/rdd/reduce/ReduceExample.scala
@@ -0,0 +1,20 @@
+package com.sparkTutorial.rdd.reduce;
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext;
+
+object ReduceExample {
+
+    def main(args: Array[String]) {
+        Logger.getLogger("org").setLevel(Level.OFF)
+        val conf = new SparkConf().setAppName("reduce").setMaster("local[*]")
+        val sc = new JavaSparkContext(conf)
+
+        val inputIntegers = List(1, 2, 3, 4, 5)
+        val integerRdd = sc.parallelize(inputIntegers)
+
+        val product = integerRdd.reduce((x, y) => x * y)
+        println("product is :" + product)
+    }
+}
