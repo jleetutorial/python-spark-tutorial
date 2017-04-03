3ec13255679337f94c7826709dc437d8131afbb0
James Lee <jameleetutorial@gmail.com>
2017-04-03 22:56:55 +0100

    adopt CountExample

---
+++ src/main/scala/com/sparkTutorial/rdd/count/CountExample.scala
@@ -0,0 +1,24 @@
+package com.sparkTutorial.rdd.count
+
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
+
+object CountExample {
+
+    def main(args: Array[String]) {
+        Logger.getLogger("org").setLevel(Level.ERROR)
+        val conf = new SparkConf().setAppName("count").setMaster("local[*]")
+        val sc = new JavaSparkContext(conf)
+
+        val inputWords = List("spark", "hadoop", "spark", "hive", "pig", "cassandra", "hadoop")
+        val wordRdd = sc.parallelize(inputWords)
+        println("Count: " + wordRdd.count())
+
+        val wordCountByValue = wordRdd.countByValue()
+        println("CountByValue:")
+
+        for ((word, count) <- wordCountByValue) println(word + " : " + count)
+    }
+}
