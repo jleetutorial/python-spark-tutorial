434c98fd7b9245ee8fccbc19f5413d6f020ff7a6
James Lee <jameleetutorial@gmail.com>
2017-04-05 23:16:45 +0100

    adopt AverageHousePriceSolution

---
+++ src/main/scala/com/sparkTutorial/pairRdd/aggregation/combinebykey/AverageHousePriceSolution.scala
@@ -0,0 +1,29 @@
+package com.sparkTutorial.pairRdd.aggregation.combinebykey
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object AverageHousePriceSolution {
+
+  def main(args: Array[String]) {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val conf = new SparkConf().setAppName("wordCounts").setMaster("local[3]")
+    val sc = new SparkContext(conf)
+
+    val lines = sc.textFile("in/RealEstate.csv")
+    val cleanedLines = lines.filter(line => !line.contains("Bedrooms"))
+
+
+    val housePricePairRdd = cleanedLines.map(line => (line.split(",")(3), line.split(",")(2).toDouble))
+    val housePriceTotal = housePricePairRdd.combineByKey(createCombiner, mergeValue, mergeCombiners)
+
+    val housePriceAvg = housePriceTotal.mapValues(avgCount => avgCount._2 / avgCount._1)
+    for ((bedrooms, avgPrice) <- housePriceAvg.collect()) println(bedrooms + " : " + avgPrice)
+  }
+
+  type AvgCount = (Int, Double)
+  val createCombiner = (x: Double) => (1, x)
+  val mergeValue = (avgCount: AvgCount, x: Double) => (avgCount._1 + 1, avgCount._2 + x)
+  val mergeCombiners = (avgCountA: AvgCount, avgCountB: AvgCount) => (avgCountA._1 + avgCountB._1, avgCountA._2 + avgCountB._2)
+}
