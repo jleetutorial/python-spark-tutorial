799c99357e3ad06a83b15516702d4bbd3b83f0f3
James Lee <jameleetutorial@gmail.com>
2017-04-05 23:59:13 +0100

    adopt AverageHousePriceSolution

---
+++ src/main/scala/com/sparkTutorial/pairRdd/aggregation/reducebykey/housePrice/AverageHousePriceSolution.scala
@@ -0,0 +1,27 @@
+package com.sparkTutorial.pairRdd.aggregation.reducebykey.housePrice
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object AverageHousePriceSolution {
+
+    def main(args: Array[String]) {
+        Logger.getLogger("org").setLevel(Level.ERROR)
+        val conf = new SparkConf().setAppName("avgHousePrice").setMaster("local[3]")
+        val sc = new SparkContext(conf)
+
+        val lines = sc.textFile("in/RealEstate.csv")
+        val cleanedLines = lines.filter(line => !line.contains("Bedrooms"))
+
+        val housePricePairRdd = cleanedLines.map(line => (line.split(",")(3), (1, line.split(",")(2).toDouble)))
+
+        val housePriceTotal = housePricePairRdd.reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2))
+
+        println("housePriceTotal: ")
+        for ((bedroom, total) <- housePriceTotal.collect()) println(bedroom + " : " + total)
+
+      val housePriceAvg = housePriceTotal.mapValues(avgCount => avgCount._2 / avgCount._1)
+        println("housePriceAvg: ")
+        for ((bedroom, avg) <- housePriceAvg.collect()) println(bedroom + " : " + avg)
+    }
+}
