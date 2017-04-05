d31ec283dda43f0778139f4f506fad4855814252
James Lee <jameleetutorial@gmail.com>
2017-04-06 00:04:12 +0100

    adopt WordCount

---
+++ src/main/scala/com/sparkTutorial/pairRdd/aggregation/reducebykey/WordCount.scala
@@ -0,0 +1,20 @@
+package com.sparkTutorial.pairRdd.aggregation.reducebykey
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount {
+
+  def main(args: Array[String]) {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val conf = new SparkConf().setAppName("wordCounts").setMaster("local[3]")
+    val sc = new SparkContext(conf)
+
+    val lines = sc.textFile("in/word_count.text")
+    val wordRdd = lines.flatMap(line => line.split(" "))
+    val wordPairRdd = wordRdd.map(word => (word, 1))
+
+    val wordCounts = wordPairRdd.reduceByKey(_ + _)
+    for ((word, count) <- wordCounts.collect()) println(word + " : " + count)
+  }
+}
--- src/main/scala/com/sparkTutorial/pairRdd/aggregation/reducebykey/housePrice/AverageHousePriceSolution.scala
+++ src/main/scala/com/sparkTutorial/pairRdd/aggregation/reducebykey/housePrice/AverageHousePriceSolution.scala
@@ -5,23 +5,23 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object AverageHousePriceSolution {
 
-    def main(args: Array[String]) {
-        Logger.getLogger("org").setLevel(Level.ERROR)
-        val conf = new SparkConf().setAppName("avgHousePrice").setMaster("local[3]")
-        val sc = new SparkContext(conf)
+  def main(args: Array[String]) {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val conf = new SparkConf().setAppName("avgHousePrice").setMaster("local[3]")
+    val sc = new SparkContext(conf)
 
-        val lines = sc.textFile("in/RealEstate.csv")
-        val cleanedLines = lines.filter(line => !line.contains("Bedrooms"))
+    val lines = sc.textFile("in/RealEstate.csv")
+    val cleanedLines = lines.filter(line => !line.contains("Bedrooms"))
 
-        val housePricePairRdd = cleanedLines.map(line => (line.split(",")(3), (1, line.split(",")(2).toDouble)))
+    val housePricePairRdd = cleanedLines.map(line => (line.split(",")(3), (1, line.split(",")(2).toDouble)))
 
-        val housePriceTotal = housePricePairRdd.reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2))
+    val housePriceTotal = housePricePairRdd.reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2))
 
-        println("housePriceTotal: ")
-        for ((bedroom, total) <- housePriceTotal.collect()) println(bedroom + " : " + total)
+    println("housePriceTotal: ")
+    for ((bedroom, total) <- housePriceTotal.collect()) println(bedroom + " : " + total)
 
-      val housePriceAvg = housePriceTotal.mapValues(avgCount => avgCount._2 / avgCount._1)
-        println("housePriceAvg: ")
-        for ((bedroom, avg) <- housePriceAvg.collect()) println(bedroom + " : " + avg)
-    }
+    val housePriceAvg = housePriceTotal.mapValues(avgCount => avgCount._2 / avgCount._1)
+    println("housePriceAvg: ")
+    for ((bedroom, avg) <- housePriceAvg.collect()) println(bedroom + " : " + avg)
+  }
 }
