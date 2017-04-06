8d6c4d40f01c7621b211a0a4a8575f4d94a807dc
James Lee <jameleetutorial@gmail.com>
2017-04-07 00:17:33 +0100

    adopt StackOverFlowSurveyFollowUp

--- src/main/scala/com/sparkTutorial/advanced/accumulator/StackOverFlowSurveyFollowUp.scala
+++ src/main/scala/com/sparkTutorial/advanced/accumulator/StackOverFlowSurveyFollowUp.scala
@@ -4,30 +4,35 @@ import com.sparkTutorial.rdd.commons.Utils
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
 
-object StackOverFlowSurvey {
+object StackOverFlowSurveyFollowUp {
 
   def main(args: Array[String]) {
+
     Logger.getLogger("org").setLevel(Level.ERROR)
     val conf = new SparkConf().setAppName("StackOverFlowSurvey").setMaster("local[1]")
     val sparkContext = new SparkContext(conf)
 
     val total = sparkContext.longAccumulator
     val missingSalaryMidPoint = sparkContext.longAccumulator
+    val processedBytes = sparkContext.longAccumulator
 
     val responseRDD = sparkContext.textFile("in/2016-stack-overflow-survey-responses.csv")
 
     val responseFromCanada = responseRDD.filter(response => {
+
+      processedBytes.add(response.getBytes().length)
       val splits = response.split(Utils.COMMA_DELIMITER, -1)
       total.add(1)
 
       if (splits(14).isEmpty) {
         missingSalaryMidPoint.add(1)
       }
-
       splits(2) == "Canada"
+
     })
 
     println("Count of responses from Canada: " + responseFromCanada.count())
+    println("Number of bytes processed: " + processedBytes.value)
     println("Total count of responses: " + total.value)
     println("Count of responses missing salary middle point: " + missingSalaryMidPoint.value)
   }
