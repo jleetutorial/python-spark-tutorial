0b3cc0d9c5a8000a23f5113aa6d98852c30a8af1
James Lee <jameleetutorial@gmail.com>
2017-04-06 23:14:56 +0100

    adopt AirportsByCountrySolution and AirportsByCountryProblem

---
+++ src/main/scala/com/sparkTutorial/pairRdd/groupbykey/AirportsByCountryProblem.scala
@@ -0,0 +1,22 @@
+package com.sparkTutorial.pairRdd.groupbykey
+
+object AirportsByCountryProblem {
+
+  def main(args: Array[String]) {
+
+    /* Create a Spark program to read the airport data from in/airports.text,
+       output the the list of the names of the airports located in each country.
+
+       Each row of the input file contains the following columns:
+       Airport ID, Name of airport, Main city served by airport, Country where airport is located, IATA/FAA code,
+       ICAO Code, Latitude, Longitude, Altitude, Timezone, DST, Timezone in Olson format
+
+       Sample output:
+
+       "Canada", List("Bagotville", "Montreal", "Coronation", ...)
+       "Norway" : List("Vigra", "Andenes", "Alta", "Bomoen", "Bronnoy",..)
+       "Papua New Guinea",  List("Goroka", "Madang", ...)
+       ...
+     */
+  }
+}
---
+++ src/main/scala/com/sparkTutorial/pairRdd/groupbykey/AirportsByCountrySolution.scala
@@ -0,0 +1,23 @@
+package com.sparkTutorial.pairRdd.groupbykey
+
+import com.sparkTutorial.rdd.commons.Utils
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object AirportsByCountrySolution {
+
+  def main(args: Array[String]) {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val conf = new SparkConf().setAppName("airports").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+
+    val lines = sc.textFile("in/airports.text")
+
+    val countryAndAirportNameAndPair = lines.map(airport => (airport.split(Utils.COMMA_DELIMITER)(3),
+      airport.split(Utils.COMMA_DELIMITER)(1)))
+
+    val airportsByCountry = countryAndAirportNameAndPair.groupByKey()
+
+    for ((country, airportName) <- airportsByCountry.collectAsMap()) println(country + ": " + airportName.toList)
+  }
+}
