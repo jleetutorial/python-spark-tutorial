db9190247f36d40b04abdb08220865aba2e2a868
James Lee <jameleetutorial@gmail.com>
2017-04-07 21:42:08 +0100

    improve UkMakerSpaces

--- src/main/scala/com/sparkTutorial/advanced/broadcast/UkMakerSpaces.scala
+++ src/main/scala/com/sparkTutorial/advanced/broadcast/UkMakerSpaces.scala
@@ -1,46 +1,40 @@
-//package com.sparkTutorial.advanced.broadcast
-//
-//import com.sparkTutorial.rdd.commons.Utils
-//import org.apache.log4j.{Level, Logger}
-//import org.apache.spark.{SparkConf, SparkContext}
-//
-//import scala.io.Source
-//
-//object UkMakerSpaces {
-//
-//  def main(args: Array[String]) {
-//    Logger.getLogger("org").setLevel(Level.ERROR)
-//    val conf = new SparkConf().setAppName("UkMakerSpaces").setMaster("local[1]")
-//    val sparkContext = new SparkContext(conf)
-//
-//    val postCodeMap = sparkContext.broadcast(loadPostCodeMap())
-//
-//    val makerSpaceRdd = sparkContext.textFile("in/uk-makerspaces-identifiable-data.csv")
-//
-//    val regions = makerSpaceRdd.filter(line => line.split(Utils.COMMA_DELIMITER, -1)(0) != "Timestamp")
-//      .map(line => {
-//        val postPrefix = getPostPrefix(line)
-//        if (postPrefix.isDefined && (postCodeMap.value contains postPrefix.get)) {
-//          postCodeMap.value(postPrefix.get)
-//        }
-//        "Unknown"
-//      })
-//    for ((region, count) <- regions.countByValue()) println(region + " : " + count)
-//  }
-//
-//
-//  def getPostPrefix(line: String): Option[String] = {
-//    val splits = line.split(Utils.COMMA_DELIMITER, -1)
-//    val postcode = splits(4)
-//    if (postcode.isEmpty) None else Some(postcode.split(" ")(0))
-//  }
-//
-//  def loadPostCodeMap(): Map[String, String] = {
-//    var postCodeMap = Map[String, String]()
-//    for (line <- Source.fromFile("in/uk-postcode.csv").getLines) {
-//      val splits = line.split(Utils.COMMA_DELIMITER, -1)
-//      postCodeMap += (splits(0) -> splits(7))
-//    }
-//    postCodeMap
-//  }
-//}
+package com.sparkTutorial.advanced.broadcast
+
+import com.sparkTutorial.commons.Utils
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.io.Source
+
+object UkMakerSpaces extends Serializable {
+
+  def main(args: Array[String]) {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val conf = new SparkConf().setAppName("UkMakerSpaces").setMaster("local[1]")
+    val sparkContext = new SparkContext(conf)
+
+    val postCodeMap = sparkContext.broadcast(loadPostCodeMap())
+
+    val makerSpaceRdd = sparkContext.textFile("in/uk-makerspaces-identifiable-data.csv")
+
+    val regions = makerSpaceRdd
+      .filter(line => line.split(Utils.COMMA_DELIMITER, -1)(0) != "Timestamp")
+      .filter(line => getPostPrefix(line).isDefined)
+      .map(line => postCodeMap.value.getOrElse(getPostPrefix(line).get, "Unknown"))
+
+    for ((region, count) <- regions.countByValue()) println(region + " : " + count)
+  }
+
+  def getPostPrefix(line: String): Option[String] = {
+    val splits = line.split(Utils.COMMA_DELIMITER, -1)
+    val postcode = splits(4)
+    if (postcode.isEmpty) None else Some(postcode.split(" ")(0))
+  }
+
+  def loadPostCodeMap(): Map[String, String] = {
+    Source.fromFile("in/uk-postcode.csv").getLines.map(line => {
+      val splits = line.split(Utils.COMMA_DELIMITER, -1)
+      splits(0) -> splits(7)
+    }).toMap
+  }
+}
