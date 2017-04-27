7cc0943c98b9222b3d9060ddc49f2415c0e69397
James Lee <jameleetutorial@gmail.com>
2017-04-28 00:26:10 +0100

    adapt UkMakerSpaces

---
+++ src/main/scala/com/sparkTutorial/sparkSql/join/UkMakerSpaces.scala
@@ -0,0 +1,30 @@
+package com.sparkTutorial.sparkSql.join
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{SparkSession, functions}
+
+object UkMakerSpaces {
+
+  def main(args: Array[String]) {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val session = SparkSession.builder().appName("UkMakerSpaces").master("local[*]").getOrCreate()
+
+    val makerSpace = session.read.option("header", "true").csv("in/uk-makerspaces-identifiable-data.csv")
+
+    val postCode = session.read.option("header", "true").csv("in/uk-postcode.csv")
+       .withColumn("PostCode", functions.concat_ws("", functions.col("PostCode"), functions.lit(" ")))
+
+    System.out.println("=== Print 20 records of makerspace table ===")
+    makerSpace.show()
+
+    System.out.println("=== Print 20 records of postcode table ===")
+    postCode.show()
+
+    val joined = makerSpace.join(postCode, makerSpace.col("Postcode").startsWith(postCode.col("Postcode")), "left_outer")
+
+    System.out.println("=== Group by Region ===")
+    joined.groupBy("Region").count().show(200)
+  }
+}
