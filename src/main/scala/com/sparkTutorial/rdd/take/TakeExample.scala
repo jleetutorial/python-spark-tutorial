d1e465fbcb0cd54ddbf032766035dfd313480077
James Lee <jameleetutorial@gmail.com>
2017-05-14 16:50:43 +0100

    fix typo

--- src/main/scala/com/sparkTutorial/rdd/take/TakeExample.scala
+++ src/main/scala/com/sparkTutorial/rdd/take/TakeExample.scala
@@ -1,15 +1,14 @@
 package com.sparkTutorial.rdd.take
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.SparkConf
-import org.apache.spark.api.java.JavaSparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 
 object TakeExample {
 
   def main(args: Array[String]) {
     Logger.getLogger("org").setLevel(Level.OFF)
     val conf = new SparkConf().setAppName("take").setMaster("local[*]")
-    val sc = new JavaSparkContext(conf)
+    val sc = new SparkContext(conf)
 
     val inputWords = List("spark", "hadoop", "spark", "hive", "pig", "cassandra", "hadoop")
     val wordRdd = sc.parallelize(inputWords)
