2b6fbb5771dd4b014885d6c31bad25025481f44d
James Lee <jamesleetutorial@gmail.com>
2017-09-24 11:04:15 +0100

    Update HousePriceSolution.scala

--- src/main/scala/com/sparkTutorial/sparkSql/HousePriceSolution.scala
+++ src/main/scala/com/sparkTutorial/sparkSql/HousePriceSolution.scala
@@ -5,7 +5,6 @@ import org.apache.spark.sql.SparkSession
 
 object HousePriceSolution {
 
-  val PRICE = "Price"
   val PRICE_SQ_FT = "Price SQ Ft"
 
   def main(args: Array[String]) {
@@ -13,12 +12,12 @@ object HousePriceSolution {
     Logger.getLogger("org").setLevel(Level.ERROR)
     val session = SparkSession.builder().appName("HousePriceSolution").master("local[1]").getOrCreate()
 
-    val realEstate = session.read.option("header", "true").csv("in/RealEstate.csv")
+    val realEstate = session.read
+      .option("header", "true")
+      .option("inferSchema", value = true)
+      .csv("in/RealEstate.csv")
 
-    val castedRealEstate = realEstate.withColumn(PRICE, realEstate(PRICE).cast("long"))
-      .withColumn(PRICE_SQ_FT, realEstate(PRICE_SQ_FT).cast("long"))
-
-    castedRealEstate.groupBy("Location")
+    realEstate.groupBy("Location")
       .avg(PRICE_SQ_FT)
       .orderBy("avg(Price SQ Ft)")
       .show()
