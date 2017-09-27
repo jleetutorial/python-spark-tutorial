7b40998c2217bead4d135987cf7cfc593dee6672
Pedro Bernardo <pedromb2004@hotmailcom>
2017-09-27 18:16:33 +0200

    Added rdd/collect/CollectExample.py

---
+++ rdd/collect/CollectExample.py
@@ -0,0 +1,9 @@
+from pyspark import SparkContext
+
+if __name__ == "__main__":
+    sc = SparkContext("local", "collect")
+    inputWords = ["spark", "hadoop", "spark", "hive", "pig", "cassandra", "hadoop"]
+    wordRdd = sc.parallelize(inputWords)
+    words = wordRdd.collect()
+    for word in words:
+        print(word)
